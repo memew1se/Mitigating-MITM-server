77f34628b13d2aeff51320d74b7733e5fee1ce49
Mark Likhachev <gogomecourier@gmail.com>
2024-08-07 13:31:36 +0300

    feat: Эндпоинт отправки сертификата

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
+import ssl
+import socket
+from urllib.parse import urlparse
+
+
+class Site(BaseModel):
+    url: HttpUrl
 
 
 app = FastAPI()
@@ -7,3 +16,16 @@ app = FastAPI()
 @app.get("/")
 async def get_root():
     return "OK"
+
+
+@app.post("/")
+async def post_root(site: Site):
+    parsed_url = urlparse(str(site.url))
+    hostname = parsed_url.hostname
+    port = int(parsed_url.port or 443)
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    sock = context.wrap_socket(conn, server_hostname=hostname)
+    sock.connect((hostname, port))
+
+    return ssl.DER_cert_to_PEM_cert(sock.getpeercert(True))
